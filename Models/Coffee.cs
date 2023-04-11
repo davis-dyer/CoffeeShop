089f6de0b31668f316a24d5a3761d89be211ba79
Davis Dyer <davis.dyer10@gmail.com>
2023-04-11 15:12:30 -0400

    v 1.0

---
+++ Models/Coffee.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoffeeShop.Models
+{
+    public class Coffee
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 3)] public string Name { get; set; }
+
+        [Required]
+        [StringLength(25), MinLength(3)] public string Roast { get; set; }
+
+        public int BeanVarietyId { get; set; }
+        public BeanVariety BeanVariety { get; set; }
+    }
+}
